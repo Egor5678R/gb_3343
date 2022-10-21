a224f3fc268441f18b9198338563cc5f42330fff
Vasilev Egor <egorvasilev8087@gmail.com>
2022-10-21 23:24:23 +0300

    Ex2

---
+++ hw1/ex2/Ex.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите два числа: ");
+string numberAstr = Console.ReadLine() ??"";
+string numberBstr = Console.ReadLine() ??"";
+int numberA = int.Parse(numberAstr);
+int numberB = int.Parse(numberBstr);
+Console.Write($"Большее из чисел {numberA} и {numberB}, это число ");
+if (numberA > numberB)
+{
+    Console.Write(numberA);
+}
+else
+{
+    Console.Write(numberB);
+}
