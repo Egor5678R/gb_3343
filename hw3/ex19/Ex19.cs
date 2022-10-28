d059fcf2129f697349d55679f20c2bf7302c51d3
Vasilev Egor <egorvasilev8087@gmail.com>
2022-10-28 16:22:51 +0300

    Hw3_3343

---
+++ hw3/ex19/Ex19.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine(" введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($" палидром? {number} - да");
+    }
+    else
+    {
+        Console.WriteLine($" палидром? {number} - нет");
+    }
+}
+else
+{
+    Console.WriteLine($" {number} - не пятизначное число");
+}
---
+++ hw3/ex23/Ex23.cs
@@ -0,0 +1,10 @@
+﻿int number = ReadInt(" введите число N: ");
+for (int a = 1; a <= number; a++)
+{ 
+    Console.Write($"{a*a*a} ");
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
