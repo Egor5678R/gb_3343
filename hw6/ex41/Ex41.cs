320b2b4bbf744d4806d688bc9fc798f18ecb06d6
Vasilev Egor <egorvasilev8087@gmail.com>
2022-11-19 01:18:24 +0300

    HW6

---
+++ hw6/ex41/Ex41.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите числа(незабудьте про пробел после каждого знака): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Чисел больше нуля: {count}");
+
