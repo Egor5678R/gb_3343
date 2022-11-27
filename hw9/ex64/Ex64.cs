da64dd405d6aa0441fb2c60695f08cc00f2fd5a7
Vasilev Egor <egorvasilev8087@gmail.com>
2022-11-27 23:14:53 +0300

    LastHomework

---
+++ hw9/ex64/Ex64.cs
@@ -0,0 +1,17 @@
+﻿int GetNumber(string message)
+{
+     Console.Write(message);
+     int useNum = Convert.ToInt32(Console.ReadLine());;
+     return useNum;
+}
+
+string bust(int num)
+{
+    string str = string.Empty;
+    if (num == 1) return num.ToString();
+    return num + " " + bust(num - 1);
+}
+
+int m = GetNumber("Введите m: ");
+Console.WriteLine("|\nv");
+Console.WriteLine(bust(m));
---
+++ hw9/ex66/Ex66.cs
@@ -0,0 +1,22 @@
+﻿int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int useNum = Convert.ToInt32(Console.ReadLine());
+    return useNum;
+}
+
+void PrintNum(int num, int count, int sum)
+{
+    if (count <= num)
+    {
+        sum = sum + count;
+        PrintNum(num, count + 1, sum);
+    }
+    else Console.WriteLine(sum);
+}
+
+int m = GetNumber("Введите M:");
+int n = GetNumber("Введите N:");
+int summa = 0;
+Console.WriteLine("|\nv");
+PrintNum(n, m, summa);
